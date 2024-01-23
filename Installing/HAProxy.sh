c5baf333b3d5b878b3dddb4e4e8f61101393cca2
Coby <125578099+RockiestSpy7@users.noreply.github.com>
2024-01-23 10:10:47 -0500

    Create HAProxy.sh

---
+++ Installing/HAProxy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Bash Script for installing and setting up HaProxy Load Balancer on CentOS
+
+# Your new entries to be added (each entry on a new line)
+# Adjust the IP addresses and Hostnames to your needs.
+new_entry="
+192.168.40.10    haproxy-centos8
+192.168.40.11    httpd-node01
+192.168.40.12    httpd-node02
+"
+
+# Append the new entries to the end of the hosts file
+echo "$new_entry" | sudo tee -a /etc/hosts
+
+sudo dnf update
+
+dnf install haproxy
+
+cd /etc/haproxy/
+cp haproxy.cfg haproxy.cfg-bk
