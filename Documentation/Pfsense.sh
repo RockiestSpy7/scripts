4b8b2fbeaaa11aeeed863690b035b4b4f24f904e
RockiestSpy7 <Cobyr159@gmail.com>
2024-03-02 13:23:54 -0500

    Made pfsense documentation

---
+++ Documentation/Pfsense.sh
@@ -0,0 +1,10 @@
+#-------------------------------------------#
+# Install and Configure Pfsense on CentOS 8 #
+#-------------------------------------------#
+
+## Official Documention -- https://docs.netgate.com/pfsense/en/latest/install/install-pfsense.html
+## Download ISO for Pfsense -- https://www.pfsense.org/download/
+## Installation walhthrouh -- https://docs.netgate.com/pfsense/en/latest/install/install-walkthrough.html
+
+# Virtual machines might have different requirements for installation
+
