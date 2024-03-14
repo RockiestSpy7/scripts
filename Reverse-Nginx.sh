645068afd0c8989f07098398a6bd9add97aee7b0
RockiestSpy7 <Cobyr159@gmail.com>
2024-03-14 11:13:28 -0400

    Deleted

--- Reverse-Nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# NOT Finished
-# This script installs nginx and sets up the reverse proxy
-dnf update
-dnf install nginx
-systemctl enable --now nginx
-firewall-cmd --zone=public --add-service=http --permanent
-firewall-cmd --reload
